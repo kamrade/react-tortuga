04ee8badee7d0c284a94988f330bfbb35ae051a8
Denis Michailov <denis.michailov@gmail.com>
2016-11-02 09:55:33 +1000

    Starting server. Using jade views. One root rout.

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set('views', './src/views');
+app.set('view engine', 'jade');
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: 'Books'
+	});
+});
+
+app.listen(3000, function() {
+	console.log('express server is up on port ' + port);
+});
