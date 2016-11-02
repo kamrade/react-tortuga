341a39f6da3406e894f9a22bfed9f5199aa86625
Denis Michailov <denis.michailov@gmail.com>
2016-11-02 18:05:09 +1000

    first steps in react. first component

--- public/app.js
+++ public/app.js
@@ -1,8 +1,17 @@
 console.log('main.js');
 
+var Greeter = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<h2>Hello, React!</h2>
+				<p>This is from a component</p>
+			</div>
+		);
+	}
+});
+
 ReactDOM.render(
-
-	<h2>Hello, React!</h2>,
+	<Greeter />,
 	document.getElementById('app')
-
 );
