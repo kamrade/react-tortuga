738180e2ae8e9f923259d5499b45da3cd6bfec40
Denis Michailov <kamrad.reg@gmail.com>
2016-11-02 21:04:02 +1000

    add bower. start using refs and events

--- public/app.js
+++ public/app.js
@@ -1,17 +1,39 @@
 console.log('main.js');
 
 var Greeter = React.createClass({
+	getDefaultProps: function() {
+		return {
+			name: 'React',
+			message: 'Did You kill anybody today?'
+		};
+	},
+	onButtonClick: function(e) {
+		e.preventDefault();
+		var name = this.refs.name.value;
+		console.log(name);
+	},
 	render: function() {
+		var name = this.props.name;
+		var message = this.props.message;
 		return (
 			<div>
-				<h2>Hello, React!</h2>
-				<p>This is from a component</p>
+				<h2>Hello, {name}!</h2>
+				<p>{message + '?'}</p>
+				<form onSubmit={this.onButtonClick}>
+					<input type="text" ref="name"/>
+					<button>Set Name</button>
+				</form>
 			</div>
 		);
 	}
 });
 
+var secondName ='Michailov';
+
 ReactDOM.render(
-	<Greeter />,
+	<Greeter
+		name={secondName}
+		message="How are You today"
+	/>,
 	document.getElementById('app')
 );
